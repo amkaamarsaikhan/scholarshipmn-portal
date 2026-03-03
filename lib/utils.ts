16b2ea47a89c4b0711c7348f429823396364d069
amkaamarsaikhan <amkaamarsaikhan@gmail.com>
2026-03-03 13:35:24 +0800

    telegram

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,19 +1,19 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
 export const sendTelegramNotification = async (message: string) => {
-
-  const token = "8565441529:AAGpDU-EYEyw9TSyTssq5CCga1Y-o6ggd1U"; 
-  const chatId = "8291139615"; 
+  const token = process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN;
+  const chatId = process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID;
   
+  if (!token || !chatId) {
+    console.error("Telegram credentials are missing in env!");
+    return;
+  }
+
   const url = `https://api.telegram.org/bot${token}/sendMessage`;
 
   try {
-    const response = await fetch(url, {
+    await fetch(url, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -22,12 +22,7 @@ export const sendTelegramNotification = async (message: string) => {
         parse_mode: "HTML",
       }),
     });
-
-    if (!response.ok) {
-      const errorData = await response.json();
-      console.error("Telegram API Error:", errorData);
-    }
   } catch (error) {
-    console.error("Telegram Fetch Error:", error);
+    console.error("Telegram Error:", error);
   }
 };
