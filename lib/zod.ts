9fabd173ed911a90d0bdb5e8f19596492c3d74a5
amkaamarsaikhan <amkaamarsaikhan@gmail.com>
2026-03-12 15:52:35 +0800

    zod fixed

--- lib/zod.ts
+++ lib/zod.ts
@@ -4,6 +4,7 @@ export const loginSchema = z.object({
   email: z.string().email("Хүчинтэй и-мэйл хаяг оруулна уу"),
   password: z.string().min(6, "Нууц үг дор хаяж 6 тэмдэгт байх ёстой"),
 });
+export type LoginFormValues = z.infer<typeof loginSchema>;
 
 export const scholarshipSchema = z.object({
   title: z.string().min(3, "Дор хаяж 3 тэмдэгт оруулна уу"),
